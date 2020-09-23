9110c884adc9c64d97dc9b8701c43d1145bda7f2
NicoFSwee <56690947+NicoFSwee@users.noreply.github.com>
2020-09-23 02:05:43 +0200

    Implement Calculator UI and calculator logic

--- WPF_Calculator/MainWindow.xaml.cs
+++ WPF_Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,45 @@ namespace WPF_Calculator
         public MainWindow()
         {
             InitializeComponent();
+            btn0.Click += OnButtonClicked;
+            btn1.Click += OnButtonClicked;
+            btn2.Click += OnButtonClicked;
+            btn3.Click += OnButtonClicked;
+            btn4.Click += OnButtonClicked;
+            btn5.Click += OnButtonClicked;
+            btn6.Click += OnButtonClicked;
+            btn7.Click += OnButtonClicked;
+            btn8.Click += OnButtonClicked;
+            btn9.Click += OnButtonClicked;
+            btnDelete.Click += OnButtonClicked;
+            btnDivide.Click += OnButtonClicked;
+            btnEquals.Click += OnButtonClicked;
+            btnMinus.Click += OnButtonClicked;
+            btnMultiply.Click += OnButtonClicked;
+            btnPlus.Click += OnButtonClicked;
+        }
+
+        private void OnButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if(button.Content.ToString() == "=")
+            {
+                txtIO.Text = CalculateFromString(txtIO.Text);
+            }
+            else if(button.Content.ToString() == "C")
+            {
+                txtIO.Text = string.Empty;
+            }
+            else
+            {
+                txtIO.Text += button.Content.ToString();
+            }
+        }
+
+        private string CalculateFromString(string numberString)
+        {
+            return new DataTable().Compute(numberString, null).ToString();
         }
     }
 }
